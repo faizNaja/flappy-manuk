9e63acf75ad3e470299ba424cd1c6c37c0be1e72
faizNaja <161667876+faizNaja@users.noreply.github.com>
2024-03-07 08:52:00 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1709776224,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/flappybirdbackground-sheet0.png",
+		"images/flappy__bird_1removebgpreview-sheet0.png",
+		"images/flappybirdpipe-sheet0.png",
+		"images/flappybirdpipe2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
